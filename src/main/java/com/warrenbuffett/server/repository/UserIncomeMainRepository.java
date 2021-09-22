338613200d990f2a8d3608194105003b0cfabe72
rachel5004 <j26597719@naver.com>
2021-09-22 18:15:14 +0900

    MODIFY:: modify User && UserIncomeMain Repository

--- src/main/java/com/warrenbuffett/server/repository/UserIncomeMainRepository.java
+++ src/main/java/com/warrenbuffett/server/repository/UserIncomeMainRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class UserIncomeMainRepository {
+public interface UserIncomeMainRepository extends JpaRepository<UserIncomeMain,Long>{
+
 }
--- src/main/java/com/warrenbuffett/server/repository/UserRepository.java
+++ src/main/java/com/warrenbuffett/server/repository/UserRepository.java
@@ -1,10 +1,10 @@
 package com.warrenbuffett.server.repository;
 
-import com.warrenbuffett.server.domain.UserIncomeMain;
+import com.warrenbuffett.server.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<UserIncomeMain,Long>{
+public interface UserRepository extends JpaRepository<User,String>{
 
 }

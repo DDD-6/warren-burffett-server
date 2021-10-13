955a4fd046672d10f192104728b560f95a313ad3
rachel5004 <j26597719@naver.com>
2021-10-13 23:23:37 +0900

    MODIFY:: remove unused code, add application.properties and application-oauth.properties.sample

--- src/main/java/com/warrenbuffett/server/controller/user/UserCreateRequestDto.java
+++ src/main/java/com/warrenbuffett/server/controller/user/UserCreateRequestDto.java
@@ -8,18 +8,15 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserCreateRequestDto {
-    private Long id;
     private String name;
     private String password;
     private String email;
-    private String image;
 
     public User toEntity(){
         return User.builder()
                 .user_name(name)
                 .email(email)
                 .password(password)
-                .image(image)
                 .userOauthType(UserOauthType.LOCAL)
                 .build();
     }
